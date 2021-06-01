5fa9885c4e320147ec076d9934d63c4a990e68af
Guldav <devidieego@mail.ru>
2021-06-01 20:30:25 +0300

    qwe
    qwe

---
+++ VKR/диплом/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SpeechRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 248);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(179, 189);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(13, 36);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(179, 159);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(224, 249);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.MaximumSize = new System.Drawing.Size(650, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 48);
+            this.label2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.label1.Location = new System.Drawing.Point(224, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(650, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 48);
+            this.label1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.label3.Location = new System.Drawing.Point(511, 212);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.MaximumSize = new System.Drawing.Size(650, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 48);
+            this.label3.TabIndex = 8;
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1022, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ VKR/диплом/Form1.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+using WMPLib;
+
+
+namespace SpeechRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        static Label l;
+        static Label l2;
+        static Label l3;
+
+        static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            
+            if (e.Result.Confidence > 0.55) l.Text = e.Result.Text;
+            l2.Text = ("привет братик");
+
+            WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+            wmp.URL = @"C:/Users/guldav/Desktop/диплом/Resources/привет братик.wav";
+            wmp.controls.play();
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized2);
+
+            Choices numbers = new Choices();
+            string conn = @"Data Source=DESKTOP-Q3D125Q\SQLEXPRESS;Initial Catalog=WKR;Integrated Security=True";
+            var sqlEx = String.Format("SELECT vopros FROM vopot");
+            using (SqlConnection connecti = new SqlConnection(conn))
+            {
+                connecti.Open();
+                SqlCommand command = new SqlCommand(sqlEx, connecti);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read() && reader.HasRows) 
+                {
+                    object name = reader.GetValue(0);
+                    numbers.Add(name.ToString());
+                }
+                reader.Close();
+
+                GrammarBuilder gb = new GrammarBuilder();
+                gb.Culture = ci;
+                gb.Append(numbers);
+
+                Grammar g = new Grammar(gb);
+                sre.LoadGrammar(g);
+
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+                //Thread.Sleep(TimeSpan.FromSeconds(30));//две строчки
+                //sre.RecognizeAsyncCancel();//вроде так
+            }
+        }
+        static void sre_SpeechRecognized2(object sender, SpeechRecognizedEventArgs e)
+        {
+            l2.Text = "";
+            int hu, me;
+            var db = new Entit();
+            if (e.Result.Confidence > 0.55)
+            {
+                l.Text = e.Result.Text;
+
+                string connectionString = @"Data Source=DESKTOP-Q3D125Q\SQLEXPRESS;Initial Catalog=WKR;Integrated Security=True";
+                var sqlExpression = String.Format("select * from vopot where [vopros] = '{0}'", l.Text);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read() && reader.HasRows)
+                    {
+                        object name = reader.GetValue(1);
+                        l2.Text = name.ToString();
+                        object na = reader.GetValue(2);
+                        l3.Text = na.ToString();
+                    }
+                    if (l3.Text == "NULL")
+                    {
+                        SpeechSynthesizer synth = new SpeechSynthesizer();
+                        synth.SetOutputToDefaultAudioDevice();
+                        synth.Speak(l2.Text);
+                        Console.WriteLine("1");
+                    }
+                    if (l3.Text != "NULL")
+                    {
+                        WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+                        wmp.URL = l3.Text;
+                        wmp.controls.play();
+                        Console.WriteLine("99");
+                    }
+                    if (l.Text == ("некотян выключись"))
+                    {
+                        Application.Exit();
+                    }
+                    if (l.Text == ("некотян запуск протокола чистый лист"))
+                    {
+                        Application.Exit();
+                    }
+                    if (l.Text == ("который час"))
+                    {
+                        hu = DateTime.Now.Hour ;
+                        me = DateTime.Now.Minute;
+                        l2.Text = ("точное время " + hu+":"+me + " семпай");
+                        SpeechSynthesizer synth1 = new SpeechSynthesizer();
+                        synth1.SetOutputToDefaultAudioDevice();
+                        synth1.Speak(l2.Text);
+                        
+                    }
+                }
+                var sqlExpression2 = String.Format("select * from comands where [comand] = '{0}'", l.Text); 
+                using (SqlConnection connection2 = new SqlConnection(connectionString))
+                {
+                    connection2.Open();
+                    SqlCommand command = new SqlCommand(sqlExpression2, connection2);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read() && reader.HasRows) // построчно считываем данные
+                    {
+                        object name = reader.GetValue(1);
+                        Process.Start(name.ToString());
+                    }
+                }
+                var sqlExpression3 = String.Format("select * from veb where [comands] = '{0}'", l.Text);
+                using (SqlConnection connection3 = new SqlConnection(connectionString))
+                {
+                    connection3.Open();
+                    SqlCommand command3 = new SqlCommand(sqlExpression3, connection3);
+                    SqlDataReader reader3 = command3.ExecuteReader();
+
+                    while (reader3.Read() && reader3.HasRows) 
+                    {
+                        object name = reader3.GetValue(1);
+                        Process.Start(name.ToString());
+                    }
+                }
+
+                if (l.Text == "некотян хочу посмотрет аниме")
+                {
+                    l2.Text = ("выбери жанр братик");
+                    //SpeechSynthesizer synth2 = new SpeechSynthesizer();
+                    //synth2.SetOutputToDefaultAudioDevice();
+                    //synth2.Speak(l2.Text);
+                    WindowsMediaPlayer wmp2 = new WindowsMediaPlayer();
+                    wmp2.URL = @"C:/Users/guldav/Downloads/выбери жанр.wav";
+                    wmp2.controls.play();
+
+
+                    var sqlExpression5 = String.Format("select * from veb where [comands] = '{0}'", l.Text);
+                    using (SqlConnection connection5 = new SqlConnection(connectionString))
+                    {
+                        connection5.Open();
+                        SqlCommand command5 = new SqlCommand(sqlExpression3, connection5);
+                        SqlDataReader reader5 = command5.ExecuteReader();
+
+                        while (reader5.Read() && reader5.HasRows) // построчно считываем данные
+                        {
+                            object name = reader5.GetValue(1);
+                            Process.Start(name.ToString());
+                        }
+                    }
+
+                }
+            }
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            l = label1;
+            l2 = label2;
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+          
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+          
+            Choices numbers = new Choices();
+            numbers.Add(new string[] { "привет некотян", "окей некотян", "некотян запуск"});
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = ci;
+            gb.Append(numbers);
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+            sre.RecognizeAsync(RecognizeMode.Single);
+        }
+    }
+}
